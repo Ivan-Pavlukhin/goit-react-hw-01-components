304c76ba486fe600a681b50194c0ae11b8861c1a
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-06-03 15:38:41 +0300

    added clear project

--- src/random-color.js
+++
@@ -1,6 +0,0 @@
-const randomNumber = () => Math.floor(Math.random() * (150 - 0) + 0);
-
-const randomColor = () =>
-  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-
-export default randomColor;
